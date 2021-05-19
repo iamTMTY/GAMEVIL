34c96b4c6004c2ed67173920e2d22f508fb7c19d
TMTY <salaudeen.t@gmail.com>
2021-05-19 12:29:06 +0100

    added sign-in button and changed mobile intro

--- src/Components/Pages/Home/HomeIntro.js
+++ src/Components/Pages/Home/HomeIntro.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import raiden from "../../../assets/imgs/raiden-small.jpg";
 
 export default function Intro() {
@@ -14,6 +15,10 @@ export default function Intro() {
 					<h1>
 						<pre>The Only GameStore You Need</pre>
 					</h1>
+					<Link to={"/"} className="sign-in_btn">
+						{" "}
+						SIGN IN{" "}
+					</Link>
 				</div>
 			</div>
 		</div>
